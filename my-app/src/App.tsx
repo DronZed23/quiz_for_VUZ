b46ece5a99f580af41a2bb936c79f4178d663523
DronZed23 <90638755+DronZed23@users.noreply.github.com>
2025-05-12 13:36:48 +0300

    feat:project structure

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import { StrictMode, useEffect, useState } from 'react';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { paths } from './pages/paths';
+import { MainPage } from './pages/main/mainPage';
+
+
+export const App = () => {
+
+    return (
+        <StrictMode>
+            <HashRouter>
+                <Routes>
+                    <Route path={paths.main} element={<MainPage />} />
+                    <Route path='*' element={<Navigate to={paths.main}/>} />
+                </Routes>
+            </HashRouter>
+        </StrictMode>
+    );
+};
---
+++ my-app/src/pages/main/mainPage.tsx
@@ -0,0 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+import styles from './styles.module.css';
+
+
+
+// eslint-disable-next-line react/prop-types
+export const MainPage = () => {
+   
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
