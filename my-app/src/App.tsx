36294e9901b879cb0aad9821ef10b75299a63f69
unknown <asyamehmed@mail.ru>
2025-05-12 16:41:25 +0300

    create first page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,15 +2,20 @@ import { StrictMode, useEffect, useState } from 'react';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { paths } from './pages/paths';
 import { MainPage } from './pages/main/mainPage';
+import { getDataLocalStorageQuiz } from './API/quizLocalStorage';
+import { QuizCard } from './pages/questions';
 
 
 export const App = () => {
+
+    const [step, setStep] = useState(getDataLocalStorageQuiz('step') as number || 1);
 
     return (
         <StrictMode>
             <HashRouter>
                 <Routes>
-                    <Route path={paths.main} element={<MainPage />} />
+                    <Route path={paths.questions} element={<QuizCard />} />
+                    <Route path={paths.main} element={<MainPage setStep={setStep}/>} />
                     <Route path='*' element={<Navigate to={paths.main}/>} />
                 </Routes>
             </HashRouter>
